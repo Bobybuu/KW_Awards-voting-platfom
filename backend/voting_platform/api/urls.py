418bd16defc25c3e9f011888acb6d88fd59b0619
MegaChie <113614073+MegaChie@users.noreply.github.com>
2025-02-15 13:18:26 +0000

    Outputs: - Tested: 1. Sub-category creation. 2. Sub-category listing. 3. Sub-category edit. 4. Sub-category delete. - Obsevations: 1, 2 and 4 ran successfully. 3 is not working and is always asking for input

--- backend/voting_platform/api/urls.py
+++ backend/voting_platform/api/urls.py
@@ -22,35 +22,30 @@ urlpatterns = [
     path('', include(router.urls)),
 
     path('auth/signup', CustomSignup.as_view(), name='custom-signup'),
-
     path('auth/verify-email/', views.SignupVerify.as_view(),
-        name='authemail-signup-verify'),
+         name='authemail-signup-verify'),
     path('auth/login/', views.Login.as_view(), name='authemail-login'),
     path('auth/logout/', views.Logout.as_view(), name='authemail-logout'),
-
     path('auth/password/reset/', views.PasswordReset.as_view(),
-        name='authemail-password-reset'),
-
+         name='authemail-password-reset'),
     path('auth/password/reset/verify/', views.PasswordResetVerify.as_view(),
         name='authemail-password-reset-verify'),
-    path('auth/password/reset/verified/', views.PasswordResetVerified.as_view(),
-        name='authemail-password-reset-verified'),
+    path('auth/password/reset/verified/',
+         views.PasswordResetVerified.as_view(),
+         name='authemail-password-reset-verified'),
     path('email/change/', views.EmailChange.as_view(),
          name='authemail-email-change'),
     path('email/change/verify/', views.EmailChangeVerify.as_view(),
          name='authemail-email-change-verify'),
 
     path('password/change/', views.PasswordChange.as_view(),
-        name='authemail-password-change'),
-
-
-
-
+         name='authemail-password-change'),
 
     path('categories/', CategoryListCreateView.as_view(),
          name='category-list-create'),
     path('categories/<uuid:pk>/',CategoryDetailView.as_view(),
          name='category-detail'),
+
     path("nominees/", NomineesListCreateView.as_view(),
          name="All nominees"),
     path("nominees/<uuid:pk>", NomineesListCreateView.as_view(),
@@ -59,6 +54,7 @@ urlpatterns = [
          name="Delete Nominee"),
     path("nominees/put/<uuid:pk>", NomineesUpdateView.as_view(),
          name="Nominee Edit"),
+
     path("awards/", AwardsListCreateView.as_view(),
          name="All Awards"),
     path("awards/<uuid:pk>", AwardsListCreateView.as_view(),
@@ -66,9 +62,10 @@ urlpatterns = [
     path("awards/delete/<uuid:pk>", AwardsDeleteView.as_view(),
          name="Delete Award"),
     path("awards/put/<uuid:pk>", AwardsUpdateView.as_view(),
-         name="Edit Award")
+         name="Edit Award"),
+
     path('subcategories/', SubCategoryListCreateView.as_view(),
         name='sub_category-list-create'),
     path('subcategories/<uuid:pk>/',
-         SubCategoryDetailView.as_view(), name='sub_category-detail'),
+         SubCategoryDetailView.as_view(), name='sub_category-detail')
 ]
